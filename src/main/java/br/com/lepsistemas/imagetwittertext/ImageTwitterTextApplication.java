67d06fae463a8cae793c8935df89397a1ce2e20b
Leandro Boeing Vieira <leandro.vieira@softplan.com.br>
2018-03-27 18:55:06 -0300

    First commit

---
+++ src/main/java/br/com/lepsistemas/imagetwittertext/ImageTwitterTextApplication.java
@@ -0,0 +1,12 @@
+package br.com.lepsistemas.imagetwittertext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageTwitterTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageTwitterTextApplication.class, args);
+	}
+}
---
+++ src/main/java/br/com/lepsistemas/imagetwittertext/scanner/Scanner.java
@@ -0,0 +1,7 @@
+package br.com.lepsistemas.imagetwittertext.scanner;
+
+public interface Scanner {
+	
+	public String getTextFromImage(String imageFilePath);
+
+}
